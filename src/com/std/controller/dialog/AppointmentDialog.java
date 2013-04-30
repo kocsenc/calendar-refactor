a75b90535f980f2091a29e9d310dbd05aa5f8ff8
Jenny Zhen <coffee@csh.rit.edu>
2013-04-30 15:33:18 -0400

    Refactored comments, moved class variables to top of file.

--- src/com/std/controller/dialog/AppointmentDialog.java
+++ src/com/std/controller/dialog/AppointmentDialog.java
@@ -37,6 +37,19 @@ import java.util.Date;
  * changes directly to the passed appointment.
  */
 public class AppointmentDialog extends JDialog{
+	private RefAppointment appt; // appointment that will be changed
+	private JTextField titleField; // title field component
+	private JTextField locationField; // location field component
+	private DatePanel startDatePanel; // start date field component
+	private DatePanel endDatePanel; // end date field component
+	private JLabel duration; // duration field display
+	private JTextArea description; // description field component
+	private RecurrencePattern pattern; // recurrence pattern currently selected
+	private JTextArea patternDesc; // pattern description display
+	private JToggleButton nDays; // NDaysPattern selection button
+	private JToggleButton daysOfWeek; // DaysOfWeekPattern selection button
+	private JToggleButton none; // null pattern selection button
+	private boolean returnState; // false if user canceled out of the dialog
 
 	/**
 	 * UID Used for Serializable
@@ -44,7 +57,7 @@ public class AppointmentDialog extends JDialog{
 	private static final long serialVersionUID = 5613941454015423846L;
 
 	/**
-	 * format to use when displaying date information
+	 * Format to use when displaying date information
 	 */
 	private static final SimpleDateFormat FORMAT = new SimpleDateFormat(
 			"EEE, d MMM yyyy 'at' h:mm aa");
@@ -114,17 +127,15 @@ public class AppointmentDialog extends JDialog{
 	/**
 	 * ActionListener intended to be used with the "OK" button on the
 	 * AppointmentDialog form.
-	 * <p/>
+	 *
 	 * Applies the changes of the form to the form's RefAppointment, and disposes
 	 * the form if no errors occur.
 	 *
-	 * @author xxx
 	 */
 	private class OKListener implements ActionListener{
 
 		/**
 		 * Invoked when the "OK" button is pressed.
-		 * <p/>
 		 * Applies the changes of the form to the form's RefAppointment, and disposes
 		 * the form if no errors occur.
 		 *
@@ -138,17 +149,13 @@ public class AppointmentDialog extends JDialog{
 	/**
 	 * ActionListener intended to be used with the "no recurrence" button on the
 	 * AppointmentDialog form.
-	 * <p/>
 	 * Sets the pattern to null, and updates the pattern description to match the
 	 * selection
-	 *
-	 * @author xxx
 	 */
 	private class NoPatternButtonListener implements ActionListener{
 
 		/**
 		 * Invoked when the "no recurrence" button is pressed.
-		 * <p/>
 		 * Sets the pattern to null, and updates the pattern description to match the
 		 * selection
 		 *
@@ -163,17 +170,13 @@ public class AppointmentDialog extends JDialog{
 	/**
 	 * ActionListener intended to be used with the "periodically" button on the
 	 * AppointmentDialog form.
-	 * <p/>
 	 * Prompts the user for pattern input, sets the pattern to the returned
 	 * NDaysPattern, and updates the pattern description to match the selection
-	 *
-	 * @author xxx
 	 */
 	private class NDaysButtonListener implements ActionListener{
 
 		/**
 		 * Invoked when the "periodically" button is pressed.
-		 * <p/>
 		 * Prompts the user for pattern input, sets the pattern to the returned
 		 * NDaysPattern, and updates the pattern description to match the selection
 		 *
@@ -197,18 +200,14 @@ public class AppointmentDialog extends JDialog{
 	/**
 	 * ActionListener intended to be used with the "select days" button on the
 	 * AppointmentDialog form.
-	 * <p/>
 	 * Prompts the user for pattern input, sets the pattern to the returned
 	 * DayOfWeekPattern, and updates the pattern description to match the
 	 * selection
-	 *
-	 * @author xxx
 	 */
 	private class DayOfWeekButtonListener implements ActionListener{
 
 		/**
 		 * Invoked when the "select days" button is pressed.
-		 * <p/>
 		 * Prompts the user for pattern input, sets the pattern to the returned
 		 * DayOfWeekPattern, and updates the pattern description to match the
 		 * selection
@@ -224,81 +223,13 @@ public class AppointmentDialog extends JDialog{
 
 			RecurrencePattern tempPattern =
 					DayOfWeekDialog.getPattern(getThis(), pattern);
-			if(tempPattern != null){
+			if(tempPattern != null)
 				pattern = tempPattern;
-			}
 			updatePatternDesc();
 		}
 	}
 
 	/**
-	 * the appointment that will be changed as a result of user input
-	 */
-	private RefAppointment appt;
-
-	/**
-	 * title field component
-	 */
-	private JTextField titleField;
-
-	/**
-	 * location field component
-	 */
-	private JTextField locationField;
-
-	/**
-	 * start date field component
-	 */
-	private DatePanel startDatePanel;
-
-	/**
-	 * end date field component
-	 */
-	private DatePanel endDatePanel;
-
-	/**
-	 * duration field display
-	 */
-	private JLabel duration;
-
-	/**
-	 * description field component
-	 */
-	private JTextArea description;
-
-	/**
-	 * recurrence pattern that is currently selected
-	 */
-	private RecurrencePattern pattern;
-
-	/**
-	 * pattern description display
-	 */
-	private JTextArea patternDesc;
-
-	/**
-	 * NDaysPattern selection button
-	 */
-	private JToggleButton nDays;
-
-	/**
-	 * DaysOfWeekPattern selection button
-	 */
-	private JToggleButton daysOfWeek;
-
-	/**
-	 * null pattern selection button
-	 */
-	private JToggleButton none;
-
-	/**
-	 * false if the user has canceled out of the dialog
-	 */
-	private boolean returnState;
-
-	/**
-	 * Returns false if the user has canceled out of the dialog
-	 *
 	 * @return false if the user has canceled out of the dialog
 	 */
 	private boolean getReturnState(){
