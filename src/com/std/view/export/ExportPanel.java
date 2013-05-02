a3dcc6a913ca85d3854b8172faca228adc1d8b9d
Jenny Zhen <coffee@csh.rit.edu>
2013-05-02 16:00:05 -0400

    Started working on the export GUI.

--- src/com/std/view/export/ExportPanel.java
+++ src/com/std/view/export/ExportPanel.java
@@ -8,10 +8,27 @@
 package com.std.view.export;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
-public class ExportPanel extends JFrame {
+public class ExportPanel extends JDialog {
 	public static enum EXPORT_TYPE {
 		CSV,
 		DCAL
+	}
+
+	private JButton exportButton; // button to export the calendar
+	private JButton cancelButton; // button to exit out of the export menu
+	private JLabel nameLabel; // label for the fileName text area
+	private JTextArea fileName; // text area for the file name
+	private JComboBox fileType; // drop down menu for file type
+
+	private JComboBox createOptions() {
+		fileType = new JComboBox(); // put in parameters
+		fileType.setSelectedIndex(0);
+		//fileType.addActionListener();
+		public void actionPerformed(ActionEvent e) {
+			JComboBox cb = (JComboBox)e.getSource();
+			String type = (String)cb.getSelectedItem();
+		}
 	}
 }
