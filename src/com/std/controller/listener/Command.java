92f338bcb3d23298d71b2f9022af9b211a6babb5
Kocsen Chung <kocsenc@gmail.com>
2013-05-02 15:41:05 -0400

    Made command interface

---
+++ src/com/std/controller/listener/Command.java
@@ -0,0 +1,14 @@
+package com.std.controller.listener;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kocsen
+ * Date: 5/2/13
+ * Time: 3:36 PM
+ * The command interface for all the commands related for the ssystem
+ */
+public interface Command {
+
+
+    void execute();
+}
